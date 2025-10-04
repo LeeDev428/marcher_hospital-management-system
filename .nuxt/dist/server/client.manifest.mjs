f36022a624627c9b5e6e1c9fae2be9a7644331e1
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-05 05:54:55 +0800

    Update client.manifest.mjs

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,18 +1,1 @@
-export default {
-  "@vite/client": {
-    "prefetch": true,
-    "isEntry": true,
-    "file": "@vite/client",
-    "css": [],
-    "module": true,
-    "resourceType": "script"
-  },
-  "D:/Programming/Systems/Web-Systems/Nuxtjs-Nodejs-Expressjs/marcher_hospital-management-system/node_modules/nuxt/dist/app/entry.js": {
-    "resourceType": "script",
-    "module": true,
-    "prefetch": true,
-    "preload": true,
-    "isEntry": true,
-    "file": "D:/Programming/Systems/Web-Systems/Nuxtjs-Nodejs-Expressjs/marcher_hospital-management-system/node_modules/nuxt/dist/app/entry.js"
-  }
-}
+export { default } from "file:///D:/Programming/Systems/Web-Systems/Nuxtjs-Nodejs-Expressjs/marcher_hospital-management-system/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
