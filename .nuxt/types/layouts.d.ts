40e46c7a587b246b75aa86f3235efa23d9239ff7
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-05 04:22:15 +0800

    Update layouts.d.ts

--- .nuxt/types/layouts.d.ts
+++ .nuxt/types/layouts.d.ts
@@ -1,5 +1,5 @@
 import type { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "admin" | "default" | "partner" | "patient" | "staff"
+export type LayoutKey = "admin" | "auth" | "default" | "partner" | "patient" | "staff"
 declare module 'nuxt/app' {
   interface PageMeta {
     layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
