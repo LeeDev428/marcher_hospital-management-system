0be30dc1727f0b4643cab07a7a8e4540d8285906
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-15 22:26:08 +0800

    Update index.vue

--- pages/staff/pharmacy/categories/index.vue
+++ pages/staff/pharmacy/categories/index.vue
@@ -28,7 +28,7 @@ onMounted(() => {
 							Back
 						</Button>
 					</NuxtLink>
-					<NuxtLink to="/pharmacy/categories/new">
+					<NuxtLink to="/staff/pharmacy/categories/new">
 						<Button variant="outline" size="icon">
 							<Icon name="mdi:plus" />
 						</Button>
