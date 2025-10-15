d74bb3caecb9ebe988b73a47ff96a17bb6a6154d
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-15 11:48:44 +0800

    Update index.ts

--- server/trpc/routers/pharmacy/index.ts
+++ server/trpc/routers/pharmacy/index.ts
@@ -3,10 +3,12 @@ import { pharmacyBrandsRouter } from "./pharmacyBrands"
 import { pharmacyCategoriesRouter } from "./pharmacyCategories"
 import { pharmacyItemsRouter } from "./pharmacyItems"
 import { pharmacySuppliersRouter } from "./pharmacySuppliers"
+import { pharmacySalesRouter } from "./pharmacySales"
 
 export const pharmacyRouter = createTRPCRouter({
 	brands: pharmacyBrandsRouter,
 	categories: pharmacyCategoriesRouter,
 	items: pharmacyItemsRouter,
 	suppliers: pharmacySuppliersRouter,
+	sales: pharmacySalesRouter,
 })
