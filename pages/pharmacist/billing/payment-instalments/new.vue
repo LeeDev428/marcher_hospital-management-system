fda514d57ff9f99e88010da7c117e30d5d6d236b
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-21 18:37:50 +0800

    Create new.vue

---
+++ pages/pharmacist/billing/payment-instalments/new.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useBreadcrumbsStore } from "@/stores/app"
+import BillingPaymentInstalmentForm from "../components/BillingPaymentInstalmentForm.vue"
+
+const breadcrumbsStore = useBreadcrumbsStore()
+
+onMounted(() => {
+  breadcrumbsStore.setBreadcrumbs([
+    { label: "Billing", link: "/billing" },
+    { label: "Payment Instalments", link: "/billing/payment-instalments" },
+    { label: "New Payment Instalment", link: "/billing/payment-instalments/new" },
+  ])
+})
+</script>
+
+<template>
+  <div class="h-full w-full flex flex-col gap-4 bg-white p-4 rounded-lg">
+      <BillingPaymentInstalmentForm />
+    </div></template>
