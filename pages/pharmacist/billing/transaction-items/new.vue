e1c0f6d6748fc2c557ddd99cb59ea4b4f437589a
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-21 18:37:23 +0800

    Create new.vue

---
+++ pages/pharmacist/billing/transaction-items/new.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useBreadcrumbsStore } from "@/stores/app"
+import BillingTransactionItemForm from "../components/BillingTransactionItemForm.vue"
+
+const breadcrumbsStore = useBreadcrumbsStore()
+
+onMounted(() => {
+  breadcrumbsStore.setBreadcrumbs([
+    { label: "Billing", link: "/billing" },
+    { label: "Transaction Items", link: "/billing/transaction-items" },
+    { label: "New Transaction Item", link: "/billing/transaction-items/new" },
+  ])
+})
+</script>
+
+<template>
+  <div class="h-full w-full flex flex-col gap-4 bg-white p-4 rounded-lg">
+      <BillingTransactionItemForm />
+    </div></template>
