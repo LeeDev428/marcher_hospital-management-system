35cf3e2fec09811a3691edcb88187e8e9aef0824
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-21 18:38:01 +0800

    Create [pharmacyCategoryId].vue

---
+++ pages/pharmacist/pharmacy/categories/[pharmacyCategoryId].vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useBreadcrumbsStore } from "@/stores/app"
+import PharmacyCategoryForm from "../components/PharmacyCategoryForm.vue"
+
+definePageMeta({
+  layout: 'pharmacist',
+  middleware: ['staff-type']
+})
+
+const breadcrumbsStore = useBreadcrumbsStore()
+const { pharmacyCategoryId } = useRoute().params as { pharmacyCategoryId: string }
+
+onMounted(() => {
+	breadcrumbsStore.setBreadcrumbs([
+		{ label: "Pharmacy", link: "/pharmacist/pharmacy" },
+		{ label: "Pharmacy Categories", link: "/pharmacist/pharmacy/categories" },
+		{ label: "Pharmacy Category", link: `/pharmacist/pharmacy/categories/${pharmacyCategoryId}` },
+	])
+})
+</script>
+
+<template>
+	<div class="h-full w-full flex flex-col gap-4 bg-white p-4 rounded-lg">
+		<PharmacyCategoryForm :pharmacy-category-id="pharmacyCategoryId" />
+	</div>
+</template>
