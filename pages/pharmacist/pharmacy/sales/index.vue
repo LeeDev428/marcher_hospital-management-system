06d6988a2d88b99ad5fb7ae26b584941ecb69c91
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-21 18:36:02 +0800

    Create index.vue

---
+++ pages/pharmacist/pharmacy/sales/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// Redirect pharmacy sales to billing sales for reusability
+definePageMeta({
+  middleware: () => {
+    return navigateTo('/pharmacist/billing/sales')
+  }
+})
+</script>
+
+<template>
+  <div>Redirecting to Sales History...</div>
+</template>
