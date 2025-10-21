fb160231f5cf7a5493a11b8cd7092951e631bc8b
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-21 18:36:09 +0800

    Create new.vue

---
+++ pages/pharmacist/pharmacy/new.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useBreadcrumbsStore } from "@/stores/app"
+import PharmacyItemForm from "./components/PharmacyItemForm.vue"
+
+const breadcrumbsStore = useBreadcrumbsStore()
+
+onMounted(() => {
+	breadcrumbsStore.setBreadcrumbs([
+		{ label: "Pharmacy", link: "/pharmacist/pharmacy" },
+		{ label: "New Pharmacy Item", link: "/pharmacist/pharmacy/new" },
+	])
+})
+
+useHead({
+  title: 'Staff Pharmacy'
+})
+
+</script>
+
+<template>
+	<div class="h-full w-full flex flex-col gap-4 bg-white p-4 rounded-lg overflow-y-auto">
+			<PharmacyItemForm />
+		</div></template>
