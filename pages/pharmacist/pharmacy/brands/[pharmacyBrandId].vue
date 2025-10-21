7e5e2b99e34db1e8e82d2b568e833cd384b948f7
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-21 18:37:08 +0800

    Create [pharmacyBrandId].vue

---
+++ pages/pharmacist/pharmacy/brands/[pharmacyBrandId].vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useBreadcrumbsStore } from "@/stores/app"
+import PharmacyBrandForm from "../components/PharmacyBrandForm.vue"
+
+definePageMeta({
+  layout: 'pharmacist',
+  middleware: ['staff-type']
+})
+
+const breadcrumbsStore = useBreadcrumbsStore()
+const { pharmacyBrandId } = useRoute().params as { pharmacyBrandId: string }
+
+onMounted(() => {
+	breadcrumbsStore.setBreadcrumbs([
+		{ label: "Pharmacy", link: "/pharmacist/pharmacy" },
+		{ label: "Pharmacy Brands", link: "/pharmacist/pharmacy/brands" },
+		{ label: "Pharmacy Brand", link: `/pharmacist/pharmacy/brands/${pharmacyBrandId}` },
+	])
+})
+</script>
+
+<template>
+	<div class="h-full w-full flex flex-col gap-4 bg-white p-4 rounded-lg">
+		<PharmacyBrandForm :pharmacy-brand-id="pharmacyBrandId" />
+	</div>
+</template>
