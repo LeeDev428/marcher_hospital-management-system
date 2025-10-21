8faff411b1e334f4670f2833e01f773803b14ad4
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-21 18:36:07 +0800

    Create pos.vue

---
+++ pages/pharmacist/pharmacy/pos.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// Redirect pharmacy POS to pharmacist billing POS
+definePageMeta({
+  middleware: () => {
+    return navigateTo('/pharmacist/billing')
+  }
+})
+</script>
+
+<template>
+  <div>Redirecting to Billing POS...</div>
+</template>
