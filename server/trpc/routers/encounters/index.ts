03110f90e3238b5a6db50c7fbd7d5591d308c626
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-21 18:44:57 +0800

    Update index.ts

--- server/trpc/routers/encounters/index.ts
+++ server/trpc/routers/encounters/index.ts
@@ -1,8 +1,10 @@
 import { createTRPCRouter } from "../../init"
 import { inpatientEncountersRouter } from "./inpatientEncounters"
 import { outpatientEncountersRouter } from "./outpatientEncounters"
+import { particularCatalogueRouter } from "./particularCatalogue"
 
 export const encountersRouter = createTRPCRouter({
 	inpatient: inpatientEncountersRouter,
 	outpatient: outpatientEncountersRouter,
+	particulars: particularCatalogueRouter,
 })
