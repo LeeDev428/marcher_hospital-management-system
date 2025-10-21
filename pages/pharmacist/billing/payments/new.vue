ac7926c45c6af81d55dcc27e68060b0f0034f9d5
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-21 18:37:37 +0800

    Create new.vue

---
+++ pages/pharmacist/billing/payments/new.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useBreadcrumbsStore } from "@/stores/app"
+import BillingPaymentForm from "../components/BillingPaymentForm.vue"
+
+const breadcrumbsStore = useBreadcrumbsStore()
+
+onMounted(() => {
+  breadcrumbsStore.setBreadcrumbs([
+    { label: "Billing", link: "/billing" },
+    { label: "Payments", link: "/billing/payments" },
+    { label: "New Payment", link: "/billing/payments/new" },
+  ])
+})
+</script>
+
+<template>
+  <div class="h-full w-full flex flex-col gap-4 bg-white p-4 rounded-lg">
+      <BillingPaymentForm />
+    </div></template>
