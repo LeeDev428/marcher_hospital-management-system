3c735cd04e11e3c61481785920fd543f8412b3c7
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-24 20:51:47 +0800

    Update new.vue

--- pages/pharmacist/pharmacy/suppliers/new.vue
+++ pages/pharmacist/pharmacy/suppliers/new.vue
@@ -2,18 +2,28 @@
 import { useBreadcrumbsStore } from "@/stores/app"
 import PharmacySupplierForm from "../components/PharmacySupplierForm.vue"
 
+definePageMeta({
+  layout: 'pharmacist',
+  middleware: ['staff-type']
+})
+
 const breadcrumbsStore = useBreadcrumbsStore()
+
+useHead({
+  title: 'New Supplier'
+})
 
 onMounted(() => {
 	breadcrumbsStore.setBreadcrumbs([
-		{ label: "Pharmacy", link: "/pharmacy" },
-		{ label: "Pharmacy Suppliers", link: "/pharmacy/suppliers" },
-		{ label: "New Pharmacy Supplier", link: "/pharmacy/suppliers/new" },
+		{ label: "Pharmacy", link: "/pharmacist/pharmacy" },
+		{ label: "Suppliers", link: "/pharmacist/pharmacy/suppliers" },
+		{ label: "New Supplier", link: "/pharmacist/pharmacy/suppliers/new" },
 	])
 })
 </script>
 
 <template>
 	<div class="h-full w-full flex flex-col gap-4 bg-white p-4 rounded-lg">
-			<PharmacySupplierForm />
-		</div></template>
+		<PharmacySupplierForm />
+	</div>
+</template>
