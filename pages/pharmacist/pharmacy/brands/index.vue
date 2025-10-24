a07ceafe16b17094a7893b0516a41ddc5ab072fd
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-24 20:51:59 +0800

    Update index.vue

--- pages/pharmacist/pharmacy/brands/index.vue
+++ pages/pharmacist/pharmacy/brands/index.vue
@@ -4,35 +4,46 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import PharmacyBrandsTable from "../components/PharmacyBrandsTable.vue"
 
+definePageMeta({
+  layout: 'pharmacist',
+  middleware: ['staff-type']
+})
+
 const breadcrumbsStore = useBreadcrumbsStore()
+
+useHead({
+  title: 'Pharmacy Brands'
+})
 
 onMounted(() => {
 	breadcrumbsStore.setBreadcrumbs([
-		{ label: "Pharmacy", link: "/pharmacy" },
-		{ label: "Pharmacy Brands", link: "/pharmacy/brands" },
+		{ label: "Pharmacy", link: "/pharmacist/pharmacy" },
+		{ label: "Brands", link: "/pharmacist/pharmacy/brands" },
 	])
 })
 </script>
 
 <template>
 	<div class="h-full w-full flex flex-col gap-4 bg-white p-4 rounded-lg">
-			<div class="w-full flex gap-2 justify-between">
-				<div class="w-full flex items-center gap-2">
-					<Input type="text" placeholder="Search" class="bg-white max-w-xs" />
-				</div>
-				<div class="flex gap-2">
-					<NuxtLink to="/pharmacy">
-						<Button variant="outline">
-							<Icon name="mdi:arrow-left" />
-							Back
-						</Button>
-					</NuxtLink>
-					<NuxtLink to="/pharmacist/pharmacy/brands/new">
-						<Button variant="outline" size="icon">
-							<Icon name="mdi:plus" />
-						</Button>
-					</NuxtLink>
-				</div>
+		<div class="w-full flex gap-2 justify-between">
+			<div class="w-full flex items-center gap-2">
+				<Input type="text" placeholder="Search brands" class="bg-white max-w-xs" />
 			</div>
-			<PharmacyBrandsTable />
-		</div></template>
+			<div class="flex gap-2">
+				<NuxtLink to="/pharmacist/pharmacy">
+					<Button variant="outline">
+						<Icon name="mdi:arrow-left" class="mr-2" />
+						Back to Pharmacy
+					</Button>
+				</NuxtLink>
+				<NuxtLink to="/pharmacist/pharmacy/brands/new">
+					<Button variant="default" class="bg-blue-600 hover:bg-blue-700">
+						<Icon name="mdi:plus" class="mr-2" />
+						Add Brand
+					</Button>
+				</NuxtLink>
+			</div>
+		</div>
+		<PharmacyBrandsTable />
+	</div>
+</template>
