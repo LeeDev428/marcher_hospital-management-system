c32fbc6bb6825eafe5ef8f22c1834e21c09e865c
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-31 01:38:32 +0800

    Create manage.vue

---
+++ pages/staff/encounters/[encounterId]/manage.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<p>Redirecting...</p>
+	</div>
+</template>
+
+<script setup lang="ts">
+// This page redirects to the staff encounters list
+// Individual encounter management is handled by admissions staff
+definePageMeta({
+	layout: 'staff',
+	middleware: 'auth'
+})
+
+// Redirect to encounters list
+navigateTo('/staff/encounters')
+</script>
